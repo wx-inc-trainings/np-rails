004454fc1f4d0a1e008b87bbe4422618bfe5c87f
Nicolas Palenque <nicoopalenque@gmail.com>
2021-06-01 14:33:53 -0300

    changes in spec an rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,6 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'fictium/rspec'
-require_relative './support/shared_context'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 

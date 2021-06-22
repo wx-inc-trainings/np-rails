140a5c1b35119554fc84b9c111808b20b50e17bc
Nicolas Palenque <nicoopalenque@gmail.com>
2021-06-22 20:17:11 -0300

    changes in book_suggestion_controller

--- app/controllers/api/v1/book_suggestion_controller.rb
+++ app/controllers/api/v1/book_suggestion_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
-    class BookSuggestionController < ApplicationController
+    class BookSuggestionController < ApiController
+      skip_before_action :authenticate_user!, only: [:create, :index]
       def index
         render json: BookSuggestion.all
       end

7b4b17f5dee9660fd5b62b3f8590ff934ace00a2
Nicolas Palenque <52846611+nicoopalenque@users.noreply.github.com>
2021-07-06 19:03:26 -0300

    implement pundit (#21)
    * index and show for book
    
    * changes in show method
    
    * changes in WorPaginate configs and add api_controller
    
    * changes index methods
    
    * implement rack reducer
    
    * setting reducer in book's model
    
    * solving rubocop offenses
    
    * changes in routes.rb
    
    * friendly_id implemented
    
    * changes in book's model
    
    * merge
    
    * endpoint for rent
    
    * changes in routes and response
    
    * Book ranking and active rent
    
    * Order by ascending expiration date
    
    * Changes in controller
    
    * Changes in controller
    
    * using scope
    
    * implement pundit
    
    * Solving travis conflicts
    
    * changes in create
    
    * Changes

--- app/controllers/api/v1/rent_controller.rb
+++ app/controllers/api/v1/rent_controller.rb
@@ -2,24 +2,31 @@ module Api
   module V1
     class RentController < ApiController
       def index
-        render_paginated rent_response, each_serializer: rent_serializer
+        @rents = Rent.where(user_id: params[:user_id])
+        authorize @rents
+        render_paginated @rents, each_serializer: rent_serializer
       end
 
       def create
-        new_rent = current_user.rent.new(rent_params)
-        if new_rent.save
-          render json: rent_serializer.new.serialize_to_json(new_rent), status: :created
+        rent = new_rent(params[:user_id])
+        authorize rent
+        if rent.save
+          render json: rent_serializer.new.serialize_to_json(rent), status: :created
         else
-          render json: new_rent.errors, status: :bad_request
+          render json: rent.errors, status: :bad_request
         end
       end
-      
+
       def book_ranking
-        render_paginated Book.rents_ranking, each_serializer: book_serializer
+        @books = Book.rents_ranking
+        authorize @books
+        render_paginated @books, each_serializer: book_serializer
       end
 
       def active
-        render_paginated Rent.rents_order_by_end_date(Date.current), each_serializer: rent_serializer
+        @actives = Rent.rents_order_by_end_date(Date.current)
+        authorize @actives
+        render_paginated @actives, each_serializer: rent_serializer
       end
       
       private
@@ -32,14 +39,16 @@ module Api
         )
       end
 
-      def rent_response
-        Rent.where(user_id: params[:user_id])
+      def new_rent(user_id)
+        rent = Rent.new(rent_params)
+        rent.user_id = user_id
+        rent
       end
 
       def rent_serializer
         Api::V1::Serializer::RentSerializer
       end
-      
+    
       def book_serializer
         Api::V1::Serializer::BookSerializer
       end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :null_session
   include Wor::Paginate
+  include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+  def not_authorized
+    render json: { error: 'You are not authorized to perform this action.' }, status: :unauthorized
+  end
 end

a6fe744317db7522279b00e1f5e8d7de53273399
Nicolas Palenque <52846611+nicoopalenque@users.noreply.github.com>
2021-07-16 23:48:00 -0300

    adding limit for book's title (#25)
    * index and show for book
    
    * changes in show method
    
    * changes in WorPaginate configs and add api_controller
    
    * changes index methods
    
    * implement rack reducer
    
    * setting reducer in book's model
    
    * solving rubocop offenses
    
    * changes in routes.rb
    
    * friendly_id implemented
    
    * changes in book's model
    
    * merge
    
    * endpoint for rent
    
    * changes in routes and response
    
    * Book ranking and active rent
    
    * Order by ascending expiration date
    
    * Changes in controller
    
    * Changes in controller
    
    * using scope
    
    * implement pundit
    
    * Solving travis conflicts
    
    * changes in create
    
    * mailtrap implemented
    
    * implement locales
    
    * Changes
    
    * gitignore
    
    * implement locales
    
    * sidekiq-schedule implemented
    
    * adding limit for book's title

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_07_06_204745) do
+ActiveRecord::Schema.define(version: 2021_07_13_155210) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,7 +32,7 @@ ActiveRecord::Schema.define(version: 2021_07_06_204745) do
     t.string "gender", null: false
     t.string "author", null: false
     t.string "image", null: false
-    t.string "title", null: false
+    t.string "title", limit: 25, null: false
     t.string "editor", null: false
     t.string "year", null: false
     t.datetime "created_at", precision: 6, null: false

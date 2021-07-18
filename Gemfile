76a6dc34a10942630439ae8e9ca2031f0c721780
Nicolas Palenque <nicoopalenque@gmail.com>
2021-07-18 01:23:17 -0300

    slim implemented

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem 'dotenv-rails', groups: [:development, :test]
 gem 'sidekiq-scheduler'
 gem "administrate"
 gem 'simplecov', require: false, group: :test
+gem 'slim'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -1,6 +1,9 @@
 class RentMailer < ApplicationMailer
   def rent_email
     @rent = params[:rent]
+    @book = @rent.book
+    puts "=====> #{@rent.id}"
+    puts "======> #{@book.title}"
     mail(
       to: @rent.user.email,
       subject: "#{I18n.t(:subject)}, #{@rent.user.first_name} #{@rent.user.last_name}"

1da06c93061662f1e5f90062648aa103932b0b33
Nicolas Palenque <52846611+nicoopalenque@users.noreply.github.com>
2021-07-21 14:19:06 -0300

    Puts deleted

--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -2,8 +2,6 @@ class RentMailer < ApplicationMailer
   def rent_email
     @rent = params[:rent]
     @book = @rent.book
-    puts "=====> #{@rent.id}"
-    puts "======> #{@book.title}"
     mail(
       to: @rent.user.email,
       subject: "#{I18n.t(:subject)}, #{@rent.user.first_name} #{@rent.user.last_name}"

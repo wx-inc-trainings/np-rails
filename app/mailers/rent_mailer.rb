736a4910a44a2eba4f8acfcc038503a6713a6052
Nicolas Palenque <52846611+nicoopalenque@users.noreply.github.com>
2021-07-21 16:26:40 -0300

    slim implemented (#29)
    * slim implemented
    
    * Puts deleted

--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -1,6 +1,7 @@
 class RentMailer < ApplicationMailer
   def rent_email
     @rent = params[:rent]
+    @book = @rent.book
     mail(
       to: @rent.user.email,
       subject: "#{I18n.t(:subject)}, #{@rent.user.first_name} #{@rent.user.last_name}"

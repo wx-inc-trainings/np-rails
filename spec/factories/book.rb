127eefbf1b91a18efc54cde7e6437bbda629649c
Nicolas Palenque <52846611+nicoopalenque@users.noreply.github.com>
2021-07-23 11:59:45 -0300

    simplecov implemented (#28)
    * index and show for book
    
    * changes in show method
    
    * changes in WorPaginate configs and add api_controller
    
    * changes index methods
    
    * implement rack reducer
    
    * setting reducer in book's model
    
    * solving rubocop offenses
    
    * changes in routes.rb
    
    * friendly_id implemented
    
    * changes in book's model
    
    * merge
    
    * endpoint for rent
    
    * changes in routes and response
    
    * Book ranking and active rent
    
    * Order by ascending expiration date
    
    * Changes in controller
    
    * Changes in controller
    
    * using scope
    
    * implement pundit
    
    * Solving travis conflicts
    
    * changes in create
    
    * mailtrap implemented
    
    * implement locales
    
    * implement locales
    
    * sidekiq-schedule implemented
    
    * adding limit for book's title
    
    * adding seeds.rb
    
    * changes in Faker for years between 1800 and 2021
    
    * Administrate
    
    * deploying changes
    
    * deploying changes
    
    * deploy heroku
    
    * deploy heroku
    
    * heroku deploy
    
    * deploy
    
    * views
    
    * views
    
    * deploy
    
    * deploy
    
    * deploy
    
    * deploy
    
    * views
    
    * migrations
    
    * simplecov implemented
    
    * coverage in git ignore
    
    * delete coverage folder
    
    * slim implemented (#29)
    
    * slim implemented
    
    * Puts deleted

--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -1,9 +1,13 @@
 FactoryBot.define do
+  title = Faker::Book.title
+  if title.length > 25
+    title = "#{title.first(22)}..."
+  end
   factory :book do
     gender { Faker::Book.genre }
     author { Faker::Book.author }
     image { 'image.jpg' }
-    title { Faker::Book.title }
+    title { title }
     editor { Faker::Book.publisher }
     year { '2021' }
   end
